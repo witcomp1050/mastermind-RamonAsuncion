22ce5706d9ef5a35ea196a7eb170cd39cf588738
Ramon <asuncionbatista@gmail.com>
2020-11-26 20:49:28 -0500

    Recator fxml file rename

--- src/me/ramonasuncion/Main.java
+++ src/me/ramonasuncion/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application
     public void start(Stage primaryStage) throws Exception
     {
 
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("mmind.fxml"));
         Parent root =  loader.load();
 
 
