7dd1fbc5928bcf5fcef7458030ae01443b221dda
Ramon Asuncion <asuncionbatista@gmail.com>
2020-11-27 23:44:12 -0500

    File to return white and black pegs

---
+++ src/me/ramonasuncion/ScoreFromGuess.java
@@ -0,0 +1,11 @@
+package me.ramonasuncion;
+
+public class ScoreFromGuess {
+
+    // White ball = Correct color in the wrong position
+    // Black ball = Correct color in the correct position
+    // the masterminds feedback does not match the order of the balls on the board
+
+
+    // Make a get the black pegs method and a white one here
+}
