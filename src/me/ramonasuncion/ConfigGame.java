7b9dac3355866569972d8ad32e595866b64dd31c
Ramon Asuncion <asuncionbatista@gmail.com>
2020-11-27 23:10:22 -0500

    Configuration file to be modified

---
+++ src/me/ramonasuncion/ConfigGame.java
@@ -0,0 +1,20 @@
+package me.ramonasuncion;
+
+import org.apache.commons.configuration2.PropertiesConfiguration;
+
+public class ConfigGame
+{
+
+    public ConfigGame(String propertiesFileName)
+    {
+
+    }
+
+
+
+    //
+    private int codeLength;
+    private boolean duplicationsAreAllowed;
+    private boolean blanksAreAllowed;
+    private PropertiesConfiguration config;
+}
