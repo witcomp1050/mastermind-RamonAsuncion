dd0ab7ad03acc37546d782c3ae7c7d91e79905ca
Ramon Asuncion <asuncionbatista@gmail.com>
2020-11-30 15:16:13 -0500

    Default added

--- src/me/ramonasuncion/Configuration.java
+++ src/me/ramonasuncion/Configuration.java
@@ -1,6 +1,8 @@
 package me.ramonasuncion;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -9,6 +11,14 @@ import org.apache.commons.configuration2.builder.fluent.Configurations;
 
 public class Configuration
 {
+    // Member variables
+    private boolean duplicationsAreAllowed;
+    private boolean blanksAreAllowed;
+    private int codeLength;
+    private int numberOfRows;
+    private PropertiesConfiguration config;
+    private File configFile;
+
     // Get the properties file
     public Configuration(String propertiesFileName)
     {
@@ -17,7 +27,7 @@ public class Configuration
             configFile = new File(getClass().getResource(propertiesFileName).getFile());
             if (!configFile.exists() && configFile.isDirectory())
             {
-                return;
+                defaults();
             }
             else {
                 Configurations configuration = new Configurations();
@@ -28,22 +38,39 @@ public class Configuration
             System.exit(1);
         }
         //</editor-fold>
+
     }
 
-
+    // Default if properties fails
+    public void defaults()
+    {
+        //<editor-fold desc="...">
+        try
+        {
+            FileWriter myWriter = new FileWriter("assets/mmind.properties");
+            myWriter.write("# Mastermind Properties File (mmind.properties)\n" +
+                    "codeSize = 4\n" +
+                    "codePegRows = 10\n" +
+                    "dupsAllowedInCode = true\n" +
+                    "blanksAllowedInCode = false\n" +
+                    "playSounds = true\n" +
+                    "maxTime = 0");
+            myWriter.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error unoccured in creating default file ");
+            e.printStackTrace();
+        }
+        //</editor-fold>
+    }
 
     // (Encapsulation) Methods to get the variable value of private class variables.
     public boolean isDuplicationsAreAllowed() { return duplicationsAreAllowed; }
+
     public boolean isBlanksAreAllowed() { return blanksAreAllowed; }
+
     public int getCodeLength() { return codeLength; }
+
     public int getNumberOfRows() { return numberOfRows; }
-
-
-    // Member variables
-    private boolean duplicationsAreAllowed;
-    private boolean blanksAreAllowed;
-    private int codeLength;
-    private int numberOfRows;
-    private PropertiesConfiguration config;
-    private File configFile;
 }
