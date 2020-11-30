5435b9766de472d589b95729dd1ccbd6b69d6463
Ramon Asuncion <asuncionbatista@gmail.com>
2020-11-30 12:02:33 -0500

    Removed old code stored in Main.java

--- src/me/ramonasuncion/Main.java
+++ src/me/ramonasuncion/Main.java
@@ -1,10 +1,22 @@
 package me.ramonasuncion;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import static javafx.scene.layout.AnchorPane.setLeftAnchor;
+import static javafx.scene.layout.AnchorPane.setTopAnchor;
 
 public class Main extends Application
 {
@@ -15,39 +27,18 @@ public class Main extends Application
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("mmind.fxml"));
         Parent root =  loader.load();
 
-
         primaryStage.setTitle("Mastermind");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-//        CodeMaker make = new CodeMaker();
-//
-//        int[] nums = new int[] {-1, -1, -1, -1};
-//        int[] randomCode = make.codeConverstionToIntegers(make.makeRandomCode());
-//        int[] feedback = make.scoreGuess(nums, randomCode);
-//        /*
-//        Now, feedback[0] = number of black pegs
-//        feedback[1] = number of white pegs
-//          */
-//
-//        if (feedback[0]==4) { //if player has won
-//            System.out.print("WIN!");
-//        }
-//
-//        if(feedback[0] > 0){
-//            System.out.println("Hello");
-//            feedback[0]--;
-//        }
-//        if(feedback[1] > 0){
-//            System.out.println("World");
-//            feedback[1]--;
-//        }
+    public static void main(String[] args) { launch(args); }
 
-        launch(args);
+    public void exitApp(){
+
     }
+
 }
 
 
