d3f5bef56f6dfc9cdc859039d2bce74c07bd9df8
Ramon Asuncion <asuncionbatista@gmail.com>
2020-12-01 09:38:51 -0500

    Debug of the menubar

--- src/me/ramonasuncion/Main.java
+++ src/me/ramonasuncion/Main.java
@@ -5,10 +5,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,20 +29,49 @@ public class Main extends Application
     public static void main(String[] args) { launch(args); }
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         (new CodeBreaker()).initalizeCodeBreaker();
 
-       //FXMLLoader loader = new FXMLLoader(Main.class.getResource("mmind.fxml"));
-       //Parent root = loader.load();
+        Menu file = new Menu("File");
 
-       // primaryStage.setTitle("Mastermind");
-       // primaryStage.setScene(new Scene(root));
+        MenuItem config = new MenuItem("Configuration");
+        MenuItem exit = new MenuItem("Exit");
+
+        file.getItems().addAll(config, exit);
+
+        Menu other = new Menu("Other");
+        MenuItem about = new MenuItem("About");
+
+
+        other.getItems().addAll(about);
+
+        MenuBar menuBar = new MenuBar();
+
+        menuBar.setTranslateX(200);
+        menuBar.setTranslateY(20);
+
+        menuBar.getMenus().addAll(file, other);
+
+
+        Group root = new Group(menuBar);
+        Scene scene = new Scene(root, 595, 200, Color.BEIGE);
+
+
+        primaryStage.setTitle("Menu Bar Example");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+
+        //FXMLLoader loader = new FXMLLoader(Main.class.getResource("mmind.fxml"));
+        //Parent root = loader.load();
+
+        // primaryStage.setTitle("Mastermind");
+        // primaryStage.setScene(new Scene(root));
         //.setResizable(false);
-       // primaryStage.show();
+        // primaryStage.show();
     }
-
-
 
 
     public void exitApp(){
